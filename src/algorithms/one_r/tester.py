8b05366d58873508df3e73cc540921a1cd9eeddf
Omar Beltran <carlos.beltran2959@alumnos.udg.mx>
2022-03-06 13:48:34 -0600

    add: zero_r & one_r structure

---
+++ src/algorithms/one_r/tester.py
@@ -0,0 +1,9 @@
+from src.tester.tester import Tester
+
+
+class OneRTester(Tester):
+    def test(self, model_description, test_set):
+        pass
+
+    def retrieve_test_output(self):
+        pass
