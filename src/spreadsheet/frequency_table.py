2354292f2f993137de3a62a101a7fd46ba66c732
Diego López Torres <buyeye09@gmail.com>
2022-03-19 21:10:37 -0600

    feat: creation of FrequencyTableGenerator class

---
+++ src/spreadsheet/frequency_table.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+class FrequencyTableGenerator:
+
+    def __init__(self, target_column: str):
+        self._target_column = target_column
+
+    def generate(self, spreadsheet: pd.DataFrame):
+        frequency_tables = list()
+        for column in spreadsheet.columns:
+            if column == self._target_column:
+                continue
+            frequency_table = {
+                column: spreadsheet[[column, self._target_column]].value_counts().to_dict()
+            }
+            frequency_tables.append(frequency_table)
+        return frequency_tables
