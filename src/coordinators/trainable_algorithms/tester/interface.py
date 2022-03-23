12660297182aef2b3b33a1ee6ad1f2bc2593b997
Omar Beltran <carlos.beltran2959@alumnos.udg.mx>
2022-03-22 23:40:53 -0600

    refactor: add datatype for the test_set

--- src/coordinators/trainable_algorithms/tester/interface.py
+++ src/coordinators/trainable_algorithms/tester/interface.py
@@ -1,10 +1,11 @@
 from abc import ABC, abstractmethod
+import pandas as pd
 
 
 class Tester(ABC):
 
     @abstractmethod
-    def test(self, model_description, test_set):
+    def test(self, model_description, test_set: pd.DataFrame):
         pass
 
     @abstractmethod
