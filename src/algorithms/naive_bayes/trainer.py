07048c8e88de0fbdb8bc2c1bca38591519803b24
Diego López Torres <buyeye09@gmail.com>
2022-03-24 19:27:26 -0600

    feat: calculated target column verosimilitude at naive bayes trainer

--- src/algorithms/naive_bayes/trainer.py
+++ src/algorithms/naive_bayes/trainer.py
@@ -1,10 +1,10 @@
 import pandas as pd
+from functools import reduce
+
 from src.coordinators.trainable_algorithms.trainer.interface import Trainer
 from src.tables.frequency import FrequencyTableGenerator
 from src.tables.verosimilitude.base import BaseVerosimilitudeTable
 from src.tables.verosimilitude.concrete_decorators.zero_frequency import ZeroFrequencyVerosimilitudeTable
-from pprint import pprint
-
 
 class NaiveBayesTrainer(Trainer):
 
@@ -16,14 +16,10 @@ class NaiveBayesTrainer(Trainer):
         self.__avgs_and_stdevps = list()
         self.__model_description = dict()
 
-    # 1. Calculate frequency tables
-    # 2. Calculate verosimilitude tables
-        # 2.1 If zero frequency, recalculate adding 1 to all elements
-    # 3. Calculate avgs and stdevps
-    # 4. Calculate posterior probabilities (tester)
     def train(self, spreadsheet: pd.DataFrame):
         self.__generate_verosimilitude_tables(spreadsheet)
-        self.__generate_avgs_and_stdevps(spreadsheet)
+        self.__calculate_avgs_and_stdevps(spreadsheet)
+        self.__calculate_target_column_verosimilitude(spreadsheet)
 
     def __generate_verosimilitude_tables(self, spreadsheet: pd.DataFrame):
         categorical_attributes = spreadsheet.select_dtypes(include=['object'])
@@ -41,7 +37,7 @@ class NaiveBayesTrainer(Trainer):
                 continue
             self.__model_description[column] = self.__verosimilitude_tables.get_table_for(column)
 
-    def __generate_avgs_and_stdevps(self, spreadsheet: pd.DataFrame):
+    def __calculate_avgs_and_stdevps(self, spreadsheet: pd.DataFrame):
         numeric_attributes = spreadsheet.select_dtypes(include=['number'])
         numeric_attributes[self.__target_column] = spreadsheet[self.__target_column]
 
@@ -54,6 +50,19 @@ class NaiveBayesTrainer(Trainer):
             self.__model_description[column] = {}
             self.__model_description[column]['avg'] = list(avg.values())[0]
             self.__model_description[column]['std'] = list(std_devps.values())[0]
+
+    def __calculate_target_column_verosimilitude(self, spreadsheet: pd.DataFrame):
+        frequencies = {}
+        self.__model_description[self.__target_column] = {}
+
+        attribute_values = spreadsheet[self.__target_column].unique()
+        for attribute_value in attribute_values:
+            attribute_count = spreadsheet[spreadsheet[self.__target_column] == attribute_value].count()[0]
+            frequencies[attribute_value] = attribute_count
+
+        frequency_sum = reduce(lambda x, y: x + y, frequencies.values())
+        similitude = {key: value / frequency_sum for key, value in frequencies.items()}
+        self.__model_description[self.__target_column] = similitude
 
     def retrieve_model_description(self):
         return self.__model_description
