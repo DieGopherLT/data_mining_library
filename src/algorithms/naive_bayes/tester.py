441b60a4d91c04db4a2ede3f5ec7322357436555
Omar Beltran <carlos.beltran2959@alumnos.udg.mx>
2022-03-27 15:19:42 -0600

    feat: grouping instances to be normalized among them

--- src/algorithms/naive_bayes/tester.py
+++ src/algorithms/naive_bayes/tester.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 import pandas as pd
 from scipy.stats import norm
 from functools import reduce
@@ -13,12 +15,14 @@ class NaiveBayesTester(Tester):
 
     def test(self, model_description: dict, test_set: pd.DataFrame):
         self.__model_description = model_description
-        self.__calculate_probability(test_set)
+        probabilities = self.__calculate_probabilities(test_set)
+        # print(probabilities)
+        self.__normalize(probabilities, test_set)
 
     def retrieve_test_output(self):
         pass
 
-    def __calculate_probability(self, test_set: pd.DataFrame):
+    def __calculate_probabilities(self, test_set: pd.DataFrame):
         columns = list(test_set.columns)
         enum_column = list(enumerate(test_set.columns))
         enum_column = {column: index for [index, column] in enum_column}
@@ -51,14 +55,30 @@ class NaiveBayesTester(Tester):
                         factors[column] = pdf
 
                     else:
-                        factors[column] = self.__model_description[column][target_column_value][getattr(instance, f'_{attr}')]
+                        factors[column] = self.__model_description[column][target_column_value][
+                            getattr(instance, f'_{attr}')]
 
-                probability = reduce(lambda x, y: x*y, factors.values())
+                probability = reduce(lambda x, y: x * y, factors.values())
 
                 factors["probability"] = probability
 
                 row = pd.DataFrame(
                     factors
-                , index=[f'Pr [ { i } | {target_column_value} ]'])
+                    , index=[f'Pr [ {i} | {target_column_value} ]'])
 
                 output_df = pd.concat([output_df, row])
+
+        return output_df
+
+    def __normalize(self, probabilities: pd.DataFrame, test_set: pd.DataFrame):
+        target_column_values = test_set[self.__target_column].unique()
+        target_column_values_count = len(target_column_values)
+        rows_to_normalize = int(len(probabilities) / target_column_values_count)
+
+        instance_group = list()
+        for row in range(1, rows_to_normalize + 1):
+            instance_group = [f'Pr [ {row} | {column_value} ]' for column_value in target_column_values]
+            pprint(instance_group)
+
+        grouped_instances = probabilities.loc[instance_group]
+        print(grouped_instances)
