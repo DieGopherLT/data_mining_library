16eb7ac5e87950a4de8b75480228ea07e9b2a8e3
Omar Beltran <carlos.beltran2959@alumnos.udg.mx>
2022-04-18 19:21:15 -0500

    fix: avoiding circular imports by changing type annotations

--- src/mediator/base_component.py
+++ src/mediator/base_component.py
@@ -3,7 +3,7 @@ from .interface import MachineLearningMediator
 
 class MediableComponent:
 
-    def __init__(self, mediator: MachineLearningMediator):
+    def __init__(self, mediator: MachineLearningMediator = None):
         self.mediator = mediator
 
     def set_mediator(self, mediator: MachineLearningMediator):
--- src/mediator/interface.py
+++ src/mediator/interface.py
@@ -1,11 +1,8 @@
 from abc import ABC, abstractmethod
 
-from .base_component import MediableComponent
-
 
 class MachineLearningMediator(ABC):
     
     @abstractmethod
-    def notify(self, sender: MediableComponent, event: str):
+    def notify(self, sender: object, event: str):
         pass
-    
